0ba1899e1773da9ed78aae11d1da4ab275445e4a
Billy Chui <billy2022002@gmail.com>
2021-11-11 09:26:19 +1100

    connect mongoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+const { connectToDB } = require('./utils/db');
+
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(PORY, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
