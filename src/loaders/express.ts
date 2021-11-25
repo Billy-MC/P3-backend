be966b60c3537c082cec29940ae69ee9ee2f0db4
Billy Chui <billy2022002@gmail.com>
2021-11-25 19:44:24 +1100

    edit the loader express

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import apiRouter from '../routes/api/';
+import routes from '../routes/api/';
 import config from '../config';
 
 export default ({ app }: any) => {
   app.use(cors());
   app.use(express.json());
-  app.use(config.api.prefix, apiRouter);
+  app.use(config.api.prefix, routes());
   return app;
 };
