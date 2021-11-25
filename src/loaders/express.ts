096c73e12c44c1714a4e1633cb81ce8f3f09042b
Billy Chui <billy2022002@gmail.com>
2021-11-25 19:56:54 +1100

    express route>apiRoute

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import routes from '../routes/api/';
+import apiRoutes from '../routes/api/';
 import config from '../config';
 
 export default ({ app }: any) => {
   app.use(cors());
   app.use(express.json());
-  app.use(config.api.prefix, routes());
+  app.use(config.api.prefix, apiRoutes());
   return app;
 };
