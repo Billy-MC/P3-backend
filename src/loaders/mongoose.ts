83b530d0e1ba95edfabf2edcf48a90d7588a8ab7
Billy Chui <billy2022002@gmail.com>
2021-11-27 13:40:35 +1100

    rewrite mongoose connection by promises

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -9,18 +9,17 @@ export default async () => {
     connectionString = `mongodb://${process.env.DB_HOST}:${config.databasePort}/${config.databaseName}`;
   }
   
-console.log(connectionString);
+  console.log(connectionString);
 
-const connect = () => {
-  mongoose.connect(connectionString)
-  .then(()=>{
-  console.log(`Successfully connected to database: ${process.env.DB_NAME}`);})
-  .catch(error => {
-    console.error('Error connecting to database: ', error);
-    return process.exit(1);
-  });
-};
-connect();
+  const connect = () => {
+    mongoose.connect(connectionString)
+    .then(()=>console.log(`Successfully connected to database: ${process.env.DB_NAME}`))
+    .catch(error => {
+      console.error('Error connecting to database: ', error);
+      return process.exit(1);
+    });
+  };
+  connect();
 
-mongoose.connection.on('disconnected', connect);
+  mongoose.connection.on('disconnected', connect);
 }
