cefbf41e379488290b3d3889741b7d88279318a2
Davin Jiang <davinjiang95@gmail.com>
2021-11-27 15:35:00 +1100

    CRUD done for user, please test on your PC

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,17 @@ const loader = require('./loaders');
 var bodyParser = require('body-parser');
 
 const app = express();
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
-app.use(bodyParser.json())
+// // parse application/json
+// app.use(bodyParser.json())
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-});
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// });
 loader.init(app);
 
 module.exports = app;
