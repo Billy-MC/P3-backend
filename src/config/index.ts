f1dcf77791d69e61c9fecbbb8203dc09118f82ef
Billy Chui <billy2022002@gmail.com>
2021-11-27 14:56:06 +1100

    rewrite the client side database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV  = process.env.NODE_ENV || 'development';
 const envFound = dotenv.config();
 if (envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
@@ -20,4 +20,10 @@ export default {
   databaseName: process.env.DB_DATABASE,
 
   databasePort: process.env.DB_PORT,
+
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost:process.env.MONGODB_HOST,
+  dbDatabase: process.env.MONGODB_DB
+
 };
--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -4,15 +4,15 @@ import config from '../config';
 export default async () => {
   let connectionString;
   if (process.env.NODE_ENV === 'production') {
-    connectionString = config.databaseURL;
+    connectionString = `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbDatabase}`;
   } else {
     connectionString = `mongodb://${process.env.DB_HOST}:${config.databasePort}/${config.databaseName}`;
   }
   
   console.log(connectionString);
 
-  const connect = () => {
-    mongoose.connect(connectionString)
+  const connect = async () => {
+    await mongoose.connect(connectionString)
     .then(()=>console.log(`Successfully connected to database: ${process.env.DB_NAME}`))
     .catch(error => {
       console.error('Error connecting to database: ', error);
