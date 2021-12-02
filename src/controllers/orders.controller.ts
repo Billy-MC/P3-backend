2943fc60002604b682e1a90d2f68a8342812fa30
samztz <1196572086@qq.com>
2021-12-02 23:06:44 +1100

    order basic CRUD complete

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,47 +1,73 @@
 import { Request, RequestHandler, Response } from 'express';
-import Order, { Status } from '../models/orders.model';
+import Order from '../models/orders.model';
 import IOrder from '../types/order.d';
-// const uuidv1 = require('uuid.v1');
 import { v1 as uuidv1 } from 'uuid';
 const Joi = require('joi');
 
 const createOrder: RequestHandler = async (req: Request, res: Response) => {
-    const { customerId, products } = req.body;
-    // const schema = Joi.object({
-    //     customer: Joi.string().required(), // TODO: check if it is UUID
-    //     produccts: Joi.array().required(), // TODO: add not empy logic, and contain only uuid        
-    //     date: Joi.date().required(),
-    //     status: Joi.String().equals        // add more lagic later
-    // })
-    const orderId:string = uuidv1();
-    const dateCreated:Date = new Date();
+  const { customerId, products } = req.body;
+  // const schema = Joi.object({
+  //     customer: Joi.string().required(), // TODO: check if it is UUID
+  //     produccts: Joi.array().required(), // TODO: add not empy logic, and contain only uuid
+  //     date: Joi.date().required(),
+  //     status: Joi.String().equals        // add more lagic later
+  // })
+  const orderId: string = uuidv1();
+  const dateCreated: Date = new Date();
+  const order: IOrder = new Order({
+    orderId,
+    customerId,
+    products,
+    dateCreated,
+    status: 'PENDING',
+  });
 
-    const order:IOrder = new Order({
-        orderId,
-        customerId,
-        products,
-        dateCreated,
-        status: Status.PENDING
-    })
+  try {
     await order.save();
-    return res.status(201).json(order);
+  } catch (e) {
+    return res.status(500).json({ error: 'server error' });
+  }
+
+  return res.status(201).json(order);
 };
 
-const getAllorders = async (req: Request, res: Response): void => {
-    const orders = await Order.find().exec();
-    return res.json(orders);
+const getAllorders = async (req: Request, res: Response) => {
+  const orders: Object = await Order.find().exec();
+  return res.json(orders);
 };
 
-const getOrderbyId = (req: Request, res: Response) => {
-    res.status(200).send('Failed');
+const getOrderbyId = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  console.log(id);
+  const order = await Order.findOne({ orderId: id });
+  if (!order) {
+    return res.status(404).json({ error: 'order not found' });
+  }
+  return res.json(order);
 };
 
-const deleteOrderById = (req: Request, res: Response) => {
-    res.status(200).send('Failed');
+const deleteOrderById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  console.log(id);
+  const order: Object = await Order.deleteOne({ orderId: id });
+  if (!order) {
+    return res.status(405).json({ error: 'order not found' });
+  }
+  return res.sendStatus(204);
 };
 
-const updateOrderById = (req: Request, res: Response) => {
-    res.status(200).send('Failed');
+const updateOrderById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { customerId, products, status } = req.body;
+  // try {
+  const order = await Order.findOneAndUpdate({ orderId: id }, { customerId, products, status }, { new: true }).exec();
+  // } catch (e) {
+  //   retrun res.status(500).json({ error:'server error'});
+  // }
+  if (!order) {
+    return res.status(405).json({ error: 'order not found.' });
+  }
+  return res.json(order);
 };
 
 export { getAllorders, getOrderbyId, deleteOrderById, updateOrderById, createOrder };
--- src/types/order.d.ts
+++ src/types/order.d.ts
@@ -1,7 +1,17 @@
+import Status from '../models';
+import Document from 'mongoose';
+
+enum status_enum {
+  PENDING,
+  COMPLETED,
+  CANCELED,
+}
+type Status = keyof typeof status_enum;
+
 export default interface IOrder extends Document {
-    orderId: string,
-    customerId: string,
-    products: [string],
-    dateCreated: Date,
-    status: Status,
+  orderId: string;
+  customerId: string;
+  products: [string];
+  dateCreated: Date;
+  status: Status;
 }
