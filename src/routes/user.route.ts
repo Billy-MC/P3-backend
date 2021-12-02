0ff61a15d9893169afa0469382e19326a486168c
Billy Chui <billy2022002@gmail.com>
2021-12-02 23:04:06 +1100

    completed login & signup

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUsers, getOneUser, deleteUser, updateUser, signUp } from '../controllers/users.controller';
+import { getUsers, getOneUser, deleteUser, updateUser, signUp, signIn } from '../controllers/users.controller';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/signup', signUp);
+router.post('/login', signIn);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
 router.get('/:id', getOneUser);
