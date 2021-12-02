5cafbddddea3f6af9a81cb97f9cd5e112c80804e
samztz <1196572086@qq.com>
2021-12-02 18:50:54 +1100

    updated create order

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,23 +1,47 @@
-import { Request, Response } from 'express';
+import { Request, RequestHandler, Response } from 'express';
+import Order, { Status } from '../models/orders.model';
+import IOrder from '../types/order.d';
+// const uuidv1 = require('uuid.v1');
+import { v1 as uuidv1 } from 'uuid';
+const Joi = require('joi');
 
-const getOrders = (req: Request, res: Response): void => {
-  res.status(200).send('Get the Orders');
+const createOrder: RequestHandler = async (req: Request, res: Response) => {
+    const { customerId, products } = req.body;
+    // const schema = Joi.object({
+    //     customer: Joi.string().required(), // TODO: check if it is UUID
+    //     produccts: Joi.array().required(), // TODO: add not empy logic, and contain only uuid        
+    //     date: Joi.date().required(),
+    //     status: Joi.String().equals        // add more lagic later
+    // })
+    const orderId:string = uuidv1();
+    const dateCreated:Date = new Date();
+
+    const order:IOrder = new Order({
+        orderId,
+        customerId,
+        products,
+        dateCreated,
+        status: Status.PENDING
+    })
+    await order.save();
+    return res.status(201).json(order);
 };
 
-const getOneOrder = (req: Request, res: Response) => {
-  res.status(200).send('Failed');
+const getAllorders = async (req: Request, res: Response): void => {
+    const orders = await Order.find().exec();
+    return res.json(orders);
 };
 
-const deleteOrder = (req: Request, res: Response) => {
-  res.status(200).send('Failed');
+const getOrderbyId = (req: Request, res: Response) => {
+    res.status(200).send('Failed');
 };
 
-const updateOrder = (req: Request, res: Response) => {
-  res.status(200).send('Failed');
+const deleteOrderById = (req: Request, res: Response) => {
+    res.status(200).send('Failed');
 };
 
-const createOrder = (req: Request, res: Response) => {
-  res.status(200).send('Failed');
+const updateOrderById = (req: Request, res: Response) => {
+    res.status(200).send('Failed');
 };
 
-export { getOrders, getOneOrder, deleteOrder, updateOrder, createOrder };
+export { getAllorders, getOrderbyId, deleteOrderById, updateOrderById, createOrder };
--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,8 +1,45 @@
-import mongoose from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const { Schema } = mongoose;
 
-const orderSchema = new Schema({});
+export enum Status {
+    PENDING ,
+    COMPLETED,
+    CANCELED 
+}
 
-const Order = mongoose.model('Order', orderSchema);
-export default Order;
+export interface IOrder extends Document {
+    orderId: string,
+    customerId: string,
+    products: [string],
+    dateCreated: Date,
+    status: Status,
+}
+
+const orderSchema = new Schema (
+    {        
+        _id: {
+            type: String,
+            alias: 'orderId',            
+        },
+        customerId: {
+            type: String,
+            required: true,
+        },
+        products: {
+            type: [String],
+            required: true,            
+        },
+        dateCreated: {
+            type: Date,
+            required: true,
+            unique: true
+        },
+        status: {
+            type: String,
+            required: true,
+        }
+    }
+);
+
+const Order = model<IOrder>('Order', orderSchema);
+export default Order
