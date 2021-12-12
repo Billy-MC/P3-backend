f42de59af89be75327f5caa50c12337703357812
DavinJ-0316 <jinhaoj@uqconnect.edu.au>
2021-12-12 16:56:08 +1100

    customer controller done

--- src/models/customers.model.ts
+++ src/models/customers.model.ts
@@ -1,55 +1,18 @@
-import { Schema, model } from 'mongoose';
-import { ICustomer } from '../types/customer';
-import { v4 as uuidv4 } from "uuid";
+import mongoose from 'mongoose';
 
-const CustomerSchema = new Schema({
-  customerId: {
-    type: String,
-    unique: true,
-  },
+const { Schema } = mongoose;
+
+const customerSchema = new Schema({
   email: {
     type: String,
     required: true,
-    lowercase: true,
-    index: true,
+    unique: true,
   },
   firstName: {
     type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  phone: {
-    type: String,
     required: true,
   },
-  DOB: {
-    type: Date,
-    required: true,
-  },
-  notification: {
-    type: [String],
-    enum: ['SMS', 'phone', 'email']
-  },
-  gender: {
-    type: String,
-    enum: ['male', 'female', 'undisclosed']
-  },
-  address: {
-    street: { type: String },
-    city: { type: String },
-    postCode: { type: String }
-  }
 });
 
-CustomerSchema.pre('save', async function customerId(next) {
-  this.customerId = await uuidv4();
-  next();
-});
-
-
-const Customer = model<ICustomer>('Customer', CustomerSchema);
-
+const Customer = mongoose.model('Customer', customerSchema);
 export default Customer;
