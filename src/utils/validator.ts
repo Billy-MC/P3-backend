f601969c3e9d731753c0c480c0afe53fe1aa2973
Billy Chui <billy2022002@gmail.com>
2022-01-16 20:12:00 +1100

    remove comment

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,23 +1,6 @@
 import Product from '@models/products.model';
 import { IProduct } from 'order';
 
-// /**
-//  * @description
-//  * @param IProducts[]
-//  * @return true if all input product's quantity is greater than inventory's(Database) quantity
-//  */
-// const productStockFullfiiled = async (products: IProduct[]): Promise<boolean> => {
-//   try {
-//     const result = products.reduce(async (prev: boolean, selectedProduct: IProduct) => {
-//       const query = await Product.findOne({ sku: String(selectedProduct.sku) });
-//       if (!query) return prev && false;
-//       return prev && query.quantity >= selectedProduct.quantity;
-//     }, true);
-//   } catch (e) {
-//     return false;
-//   }
-// };
-
 const validatePassword = (password: string): boolean => {
   const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^_&*])[a-zA-Z0-9!@#$%^_&*]{8,32}$/;
   const passwordValidataResult = passwordRegex.test(password);
