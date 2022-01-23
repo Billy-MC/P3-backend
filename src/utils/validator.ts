3dab7f5d64802207bdab0bd312f51fbe7bc02edc
Tingzhuang Zhou <tingzhuangzhou@gmail.com>
2022-01-23 14:26:57 +0000

    Merged in bugfix/DEV-170-fixed-backend-building-bug (pull request #38)
    bugfixed
    
    * bugfixed
    
    
    Approved-by: Billy Chui

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,16 +1,6 @@
 import Product from '@models/products.model';
 import { IProduct } from 'order';
 
-const inventoryFullfilled = async (products: IProduct[]): Promise<boolean> => {
-  let check = true;
-  products.forEach(async product => {
-    const inventory = await Product.findOne({ sku: String(product.sku) });
-    if (!inventory) check = false;
-    if (product.quantity > inventory.quantity) check = false;
-  });
-  return check;
-};
-
 const validatePassword = (password: string): boolean => {
   const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^_&*])[a-zA-Z0-9!@#$%^_&*]{8,32}$/;
   const passwordValidataResult = passwordRegex.test(password);
@@ -23,4 +13,4 @@ const validateEmail = (email: string): boolean => {
   return emailValidateResult;
 };
 
-export { inventoryFullfilled, validatePassword, validateEmail };
+export { validatePassword, validateEmail };
